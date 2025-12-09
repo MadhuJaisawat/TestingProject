c7f2a1c19de3cd558b032436020cd920c3dcafd1
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-09 19:40:49 +0530

    TESTING LOGOUT FUNCTION

---
+++ src/TestLogoutFn.java
@@ -0,0 +1,10 @@
+public class TestLogoutFn {
+
+    public static void main(String[] args) {
+        logout();
+    }
+
+    public static void logout() {
+        System.out.println("User logged out successfully.");
+    }
+}
